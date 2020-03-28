a332a17919502b635ed40d0f72ff53d31101f495
lordjuacs <50754596+lordjuacs@users.noreply.github.com>
2020-03-27 21:42:23 -0500

    upgrade 1

--- covid19.py
+++ covid19.py
@@ -2,10 +2,25 @@
 def comparar(file1, file2):
     f1 = open(file1, "r")
     f2 = open(file2, "r")
-    text1 = str(f1.read())
-    text2 = str(f2.read())
+    text1 = ""
+    text2 = ""
+
+    inicio1 = 0
+    for linea in f1:
+        if inicio1 != 0:
+            text1+=linea
+            inicio1+=1
+        inicio1+=1
+
+    inicio2 = 0
+    for linea in f2:
+        if inicio2 != 0:
+            text2 += linea
+            inicio2 += 1
+        inicio2 += 1
     f1.close()
     f2.close()
+
 
     menor = min(len(text1), len(text2))
     simi = 0
@@ -14,6 +29,7 @@ def comparar(file1, file2):
             simi+=1
 
     proporcion = (simi/len(text1))*100
+
     return round(proporcion,2)
 
 def imprimir(lista):
@@ -40,9 +56,7 @@ def imprimir(lista):
 
 
 #MAIN
-archivos = ["genomas/AY274119.txt", "genomas/AY278488.2.txt",
-            "genomas/MN908947.txt", "genomas/MN988668.txt",
-            "genomas/MN988669.txt"]
+archivos = ["genomas/AY274119.txt", "genomas/AY278488.2.txt", "genomas/MN908947.txt", "genomas/MN988668.txt","genomas/MN988669.txt"]
 
 imprimir(archivos)
 
